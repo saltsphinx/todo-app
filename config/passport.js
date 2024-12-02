a2e23e984d147e27ca0a3df80182b3c06aeaf814
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2024-12-01 19:02:29 -0600

    Set up passport and local strategy

---
+++ config/passport.js
@@ -0,0 +1,50 @@
+import pp from "passport";
+import LocalStrategy from "passport-local";
+import knex from "./db";
+import bcrypt from "bcryptjs";
+
+const passport = new pp.Passport();
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await knex.from("user").where("username", username).first();
+      if (!user) {
+        done(null, false, {
+          message: "Username or password entered were incorrect.",
+        });
+      }
+
+      const match = await bcrypt.compare(user.password, password);
+      if (!match) {
+        done(null, false, {
+          message: "Username or password entered were incorrect.",
+        });
+      }
+
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await knex.from("user").where("id", id).first();
+
+    if (!user) {
+      done(null, false);
+    }
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+export default passport;
